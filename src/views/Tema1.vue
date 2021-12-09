a9de4d03b4ce64b3be9d1a915699ab733cfc63e1
luisEfe80 <luismendez80@hotmail.com>
2021-12-09 06:59:48 -0500

    Adición de contenido

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -3,18 +3,18 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    .titulo-principal.color-acento-botones
+    .titulo-principal.color-acento-botones(data-aos='flip-left')
       .titulo-principal__numero
         span 1
       h1 Producción agropecuaria ecológica y agroecológica
 
-    p.mb-5  Surge como corriente o enfoque alternativo de producción, después de enfrentar las imposiciones de un modelo de <b>“Revolución verde”</b>, en el que predominaban los monocultivos, las semillas mejoradas, el uso de maquinaria agrícola, la aplicación de fertilizantes y productos agrotóxicos. Entre las consecuencias de este modelo están:
+    p.mb-5(data-aos='flip-up')  Surge como corriente o enfoque alternativo de producción, después de enfrentar las imposiciones de un modelo de <b>“Revolución verde”</b>, en el que predominaban los monocultivos, las semillas mejoradas, el uso de maquinaria agrícola, la aplicación de fertilizantes y productos agrotóxicos. Entre las consecuencias de este modelo están:
     .row.mb-5
-      .col-lg-5
-        figure
+      .col-lg-5.slideInUp
+        figure(data-aos='flip-left')
           img(src="@/assets/curso/t1-i1.jpg", alt="imagen decorativa")
       .col-lg-7
-        ul.lista-ul
+        ul.lista-ul.slideInLeft
           li 
             i.fas.fa-leaf
             p.mb-0 Ecosistemas alterados debido a los monocultivos y a los sistemas de producción intensivos.
@@ -56,36 +56,157 @@
 
     .row.mb-5.justify-content-center
       .col-lg-10
-        .cajon.color-secundario.p-4
+        .cajon.color-acento-botones.p-4
           p.mb-0  Las prácticas de manejo a favor de la biodiversidad y el cuidado del medio ambiente, la labranza mínima del suelo, la recuperación de conocimientos tradicionales, el no uso de agrotóxicos y el ciclaje de nutrientes en los agroecosistemas son algunas de las prácticas que caracterizan a estos sistemas de producción.
 
+    .neg-margin-x.mb-5.bg-curved
+      .row.justify-content-center
+        .col-lg-6
+          ImagenInfografica.color-acento-botones.mb-5
+            template(v-slot:imagen)
+              figure
+                img(src='@/assets/curso/t1-info1.svg', alt='Texto que describa la imagen')
+
+            .tarjeta.color-acento-botones.p-3(x="0%" y="48%" numero="")
+              p Según la encuesta de FiBL-IFOAM (2021), existen en el mundo un total de 72,3 millones de hectáreas en producción ecológica.
+
+            .tarjeta.color-acento-botones.p-3(x="87%" y="10%" numero="")
+              p Siendo Australia el país con mayor área (35,7 millones de hectáreas).
+
+            .tarjeta.color-acento-botones.p-3(x="97%" y="48%" numero="")
+              p Le siguen Argentina (3,7 millones de hectáreas).
+
+            .tarjeta.color-acento-botones.p-3(x="87%" y="87%" numero="")
+              p España con 2,4 millones de hectáreas.
+
+
+    #t_1_1.titulo-segundo.color-acento-botones
+      h2 1.1  Marco normativo de la producción agropecuaria ecológica
+    .row.mb-5.justify-content-center(data-aos='flip-up')
+      .col-lg-10
+        .bloque-texto-g.color-acento-contenido.p-3.p-sm-4.p-md-5
+          .bloque-texto-g__img(
+            :style="{'background-image': `url(${require('@/assets/curso/t1-i2.jpg')})`}"
+          )
+          .bloque-texto-g__texto.p-4
+            p.mb-0  En Colombia, el Ministerio de Agricultura y Desarrollo Rural (MADR) y la Federación Internacional de Movimientos de Agricultura. (IFOAM) definen los conceptos de <b>producción ecológica, biológica u orgánica, agricultura orgánica y agroecología de la siguiente manera:</b>    
+
+    //-- Aqui vienen los cards modales
+
+    p.mb-5  Es importante diferenciar estos enfoques, que son similares y buscan objetivos comunes, pero que presentan diferencias en su implementación:
+
+    .row.mb-5.justify-content-center
+      .col-lg-4
+        .tarjeta.c1.p-4.text-center.h-100(data-aos='flip-left')
+          figure.mb-3
+            img.nW.centered(src='@/assets/curso/t1-ic1.png', alt='icono decorativo')
+          p.mb-2   La agricultura ecológica, biológica u orgánica se emplea para la producción certificada por una tercera parte u organismo de certificación.
+      .col-lg-4
+        .tarjeta.c1.p-4.text-center(data-aos='flip-right')
+          figure.mb-3
+            img.nW.centered(src='@/assets/curso/t1-ic2.png', alt='icono decorativo')
+          p.mb-2   La agroecología no está regulada, y tiene un enfoque más holístico, aunque algunas veces están certificadas por una tercera parte o por un esquema alternativo.
+
+    p.mb-5  En ese sentido, el MADR expidió la normatividad de producción ecológica certificada para el uso del “Sello de Alimento Ecológico”, como se muestra en la siguiente tabla.
+    
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        figcaption.customCaption.mb-3  <b>Tabla</b> Reglamentación de la producción ecológica en Colombia
+        .tabla-a.color-acento-botones.mb-5(data-aos='slide-left') 
+          table
+            thead
+              tr
+                th.mw250  Acto administrativo
+                th Descripción
+            tbody
+              tr
+                td.text-center Resolución 0148 de 2004
+                td Por la cual se crea el Sello de Alimento Ecológico y se reglamenta su otorgamiento y uso. 
+              tr
+                td.text-center Resolución 0148 de 2004
+                td Por la cual se adopta el Reglamento para la producción primaria, procesamiento, empacado, etiquetado, almacenamiento, certificación, importación, comercialización y se establece el Sistema de Control de Productos Agropecuarios Ecológicos.
+              tr
+                td.text-center Resolución 036 de 2007
+                td Por la cual se modifica la Resolución 148 del 15 de marzo de 2004. 
+              tr
+                td.text-center Reglamento (Versión 01)
+                td  Reglamento para la producción primaria, procesamiento, empacado, etiquetado, almacenamiento, certificación, importación y comercialización de productos agropecuarios ecológicos. 
+              tr
+                td.text-center Resolución 199 de 2016
+                td Por la cual se modifica parcialmente el Reglamento para la producción primaria, procesamiento, empacado, etiquetado, almacenamiento, certificación, importación y comercialización de productos agropecuarios ecológicos – Versión 1, adoptado mediante la Resolución 0187 de 2006.
 
 
 
 
+    Separador
+    br
+    br
+    #t_1_2.titulo-segundo.color-acento-botones
+      h2 1.2  Sellos de alimentos ecológicos
+    p.mb-5  Dependiendo del origen de los alimentos se pueden clasificar como:
+    .row.mb-5
+      .col-sm-6.col-xl-4.mb-4.mb-xl-0
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .indicador--hover(v-if="indicadorTarjetaFlip")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/t1-c1.svg')})`}")
+            .tarjeta-flip__contenido.p-4.p-xl-5
+              br
+              .h4 Alimentos naturales
+              p Son los que han sido sembrados en cultivos de desarrollo natural, donde han recibido los cuidados necesarios para evitar enfermedades y plagas, a pesar de tener que recurrir a algunos pesticidas y/o sustancias químicas.    
+              br
 
+      .col-sm-6.col-xl-4.mb-4.mb-xl-0
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .indicador--hover(v-if="indicadorTarjetaFlip")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/t1-c2.svg')})`}")
+            .tarjeta-flip__contenido.p-4.p-xl-5
+              br
+              .h4 Alimentos ecológicos
+              p Se llevan a cabo con la utilización de tecnologías agroecológicas, mejorando las características de los suelos, teniendo en cuenta el  control biológico de plagas, sin recurrir a insumos químicos  y respetando la diversificación de especies, donde se valora la salud de los consumidores y del medio ambiente.
+              br
 
+      .col-sm-6.col-xl-4.mb-4.mb-xl-0
+        .tarjeta.tarjeta-flip.color-primario(@mouseover="indicadorTarjetaFlip = false")
+          .indicador--hover(v-if="indicadorTarjetaFlip")
+          .tarjeta-flip__contenedor
+            .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/t1-c2.svg')})`}")
+            .tarjeta-flip__contenido.p-4.p-xl-5
+              br
+              .h4 Alimentos transgénicos
+              p Hacen parte de un proceso de ingeniería genética, en el cual el organismo es modificado a través de la incorporación de genes de distintas especies, con el fin de engendrar nuevas características para que sea resistente a los herbicidas, plagas y adversidades del entorno.
+              br
 
+      p.mb-5  Para complementar la identificación de la procedencia de los alimentos, estos obtienen los Sellos de Alimentos.
 
+      TabsC.color-acento-botones
+        .py-3.py-md-4(titulo="Sello de Alimento <br>Ecológico del MADR")
+          .row.align-items-center
+            .col-md-6
+              figure
+                img.nW.centered(src='@/assets/curso/t1-t1.png', alt='logo MADR')          
+            .col-md-6.mb-4.mb-md-0
+              h4 Sello de Alimento Ecológico del MADR
+              p Este sello se emplea para el comercio nacional y es de observancia voluntaria, es decir, lo usan los productores que voluntariamente se certifiquen bajo la normatividad en mención.
+          
+        .py-3.py-md-4(titulo="Sellos orgánicos de Estados Unidos,<br> Japón, Unión Europea, Canadá y Corea")
+          .row.align-items-center
+            .col-md-6
+              figure
+                img.nW.centered(src='@/assets/curso/t1-t2.png', alt='ejemplos de sellos')          
+            .col-md-6.mb-4.mb-md-0
+              h4 Sellos orgánicos de Estados Unidos, Japón, Unión Europea, Canadá y Corea
+              p Los sellos internacionales de productos ecológicos son creados por cada país y son de observancia obligatoria para los productores nacionales que desean exportar sus productos.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    .row.mb-5
+      .col-lg-8(data-aos='slide-left') 
+        p.mb-4  El sello de alimento nacional fue creado con el objeto de promover la producción, la comercialización y consumo de alimentos obtenidos mediante sistemas de producción ecológica, y de proporcionar al consumidor información oportuna, confiable y suficiente para diferenciar los productos agropecuarios ecológicos de los convencionales.
+        .cajon.color-acento-botones.p-4
+          p.mb-0  Todos los países deben certificarse para obtener cada sello internacional, en el caso de Colombia a la fecha (octubre 2021), no dispone de tratados de equivalencia comercial de productos ecológicos para exportar con el Sello de Alimento Ecológico.
+      .col-lg-4(data-aos='slide-right') 
+        figure
+          img.nW.centered(src='@/assets/curso/t1-i3.jpg', alt='imagen decorativa')  
 
 
 
@@ -128,4 +249,7 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.mw250
+  max-width: 250px
+</style>
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -7,42 +7,190 @@
       .titulo-principal__numero
         span 2
       h1 Territorio
+    .row.mb-5.align-items-center.slideInLeft
+      .col-lg-3(data-aos='flip-right') 
+        figure
+          img.nW.centered(src='@/assets/curso/t2-i1.jpg', alt='imagen decorativa')      
+      .col-lg-9(data-aos='slide-left') 
+        .cajon-b.color-primario.p-3.mb-5
+          p.mb-3  Es usado en diferentes ámbitos como la Geografía, la Sociología, el urbanismo, la Arquitectura, la planificación y actualmente, se ha vuelto común en el estudio de la Agroecología.
+          p.mb-0  Este concepto ha venido evolucionando desde considerarlo solamente como un espacio físico donde interactúan los habitantes hacia una definición más compleja.  
+
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 Según la Unidad de Planificación Rural y Agropecuaria – UPRA (2018) 
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p “el territorio es un espacio geográfico en el que una persona, grupo de personas y organizaciones económicas y sociales ejercen control y dominio. La interacción de intereses y poderes de los diversos actores y organizaciones sobre el territorio configura un sistema espacial socioeconómico, institucional y político, en permanente transformación, que se interrelaciona a través de las estructuras y funcionalidades del territorio, genera sinergias y conflictos que requieren ser encauzados a través de la planificación y gestión del ordenamiento territorial, buscando transformar el crecimiento en desarrollo”.
 
 
+    p.mb-5  En el territorio se pueden identificar varias dimensiones:
+
+    TabsA.color-primario.mb-5
+      .tarjeta.tarjeta--gris.p-4(titulo="Dimensiones en el territorio")
+        .row
+          .col-lg-7
+            h4 Dimensiones en el territorio
+            p Tiene en cuenta los  recursos naturales, ecosistemas y las fuentes de energía. 
+          .col-lg-5
+            figure.slideInLeft
+              img.nW.centered(src='@/assets/curso/t2-t1.png', alt='imagen decorativa')  
+        
+      .tarjeta.tarjeta--gris.p-4(titulo="Dimensión económica")
+        .row
+          .col-lg-7
+            h4 Dimensión económica
+            p Tiene en cuenta las  formas de producción, usos del suelo, actividades económicas.
+          .col-lg-5
+            figure.slideInLeft
+              img.nW.centered(src='@/assets/curso/t2-t2.png', alt='imagen decorativa')  
+
+      .tarjeta.tarjeta--gris.p-4(titulo="Dimensión sociocultural")
+        .row
+          .col-lg-7
+            h4 Dimensión sociocultural
+            p Tiene en cuenta el recurso humano, cultura, identidad.
+          .col-lg-5
+            figure.slideInLeft
+              img.nW.centered(src='@/assets/curso/t2-t3.png', alt='imagen decorativa')  
+
+      .tarjeta.tarjeta--gris.p-4(titulo="Dimensión espacial")
+        .row
+          .col-lg-7
+            h4 Dimensión espacial
+            p Tiene en cuenta las características del espacio, rural, urbano, regional, distribución del capital social, infraestructuras.
+          .col-lg-5
+            figure.slideInLeft
+              img.nW.centered(src='@/assets/curso/t2-t4.png', alt='imagen decorativa')  
+      
+      .tarjeta.tarjeta--gris.p-4(titulo="Dimensión espacial")
+        .row
+          .col-lg-7
+            h4 Dimensión institucional
+            p Tiene en cuenta la capacidad de planificación, gestión de las entidades públicas, proyectos, empoderamiento, gobernanza.
+          .col-lg-5
+            figure.slideInLeft
+              img.nW.centered(src='@/assets/curso/t2-t5.png', alt='imagen decorativa')  
 
 
+    h3.titulo-tercero 
+      i.fas.fa-leaf
+      | &nbsp;Dimensiones del territorio
+    .row.mb-5
+      .col-lg-2
+        figure(data-aos='flip-left')
+          img.nW.centered(src='@/assets/curso/t2-t5.png', alt='imagen decorativa')          
+      .col-lg-10(data-aos='slide-left')
+        p.mb-3  El estudio del territorio, de sus dimensiones, elementos, relaciones permiten identificar diferentes variables que afectan el avance de un agroecosistema y da insumos para realizar el diagnóstico y la planificación del sistema de producción, pero también del territorio en todo su conjunto. 
+        p.mb-0  Para conocer un poco más del territorio, fuentes de información, suelo y biodiversidad, se invita a observar el siguiente video: v
+
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
+    h3.titulo-tercero 
+      i.fas.fa-leaf
+      | &nbsp;Fuentes de información
+    p.mb-5  Son todos los soportes que facilitan dar respuesta a un requerimiento de información. Estas pueden ser de diferentes tipos:
+
+    .row.mb-5.justify-content-center
+      .col-sm-6.col-xl-4.mb-4.mb-sm-0
+        .tarjeta.tarjeta-slide.derecha.color-primario(@mouseover="indicadorTarjetaSlide = false")
+          .tarjeta-slide__contenedor
+            .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/t2-c1.svg')})`}")
+            .tarjeta-slide__contenido.p-4.p-xl-5.text-center
+              .h4 Primarias
+              p Fuentes originales, como libros, artículos, tesis.
 
 
+      .col-sm-6.col-xl-4.mb-4.mb-sm-0
+        .tarjeta.tarjeta-slide.derecha.color-primario(@mouseover="indicadorTarjetaSlide = false")
+          .tarjeta-slide__contenedor
+            .tarjeta-slide__img(:style="{'background-image': `url(${require('@/assets/curso/t2-c2.svg')})`}")
+            .tarjeta-slide__contenido.p-4.p-xl-5.text-center
+              .h4 Secundarias
+              p Derivadas o producto de la interpretación de otras fuentes, como un cuadro con información estadística elaborado a partir de datos de diferentes fuentes o revistas de resúmenes.
+
+    .row.mb-5.justify-content-center
+      .col-lg-8
+        .cajon.color-acento-botones.p-4
+          p.mb-0  Se recomienda en cualquier estudio buscar en lo posible fuentes primarias fiables.
+
+    h3.titulo-tercero 
+      i.fas.fa-leaf
+      | &nbsp;Suelo
+    p.mb-5 Para entender el suelo es necesario considerar las siguientes premisas (Sánchez y Praguer, 2012):
+
+    .neg-margin-x.bg-curved.custom-dots
+      .row.justify-content-center
+        .col-lg-8
+          ImagenInfograficaB.color-acento-botones.mb-5
+            template(v-slot:imagen)
+              figure
+                img(src='@/assets/curso/t2-info1.svg', alt='infografia del suelo')
+
+            div(x="47%" y="44%" tooltip="Ver más" numero="+")
+              .tarjeta.white-card.p-4
+                .row.align-items-center
+                  .col-lg-5
+                    figure
+                      img(src='@/assets/curso/t2-if0.png', alt='imagen decorativa')
+                  .col-lg-7
+                    p Una de las tres fuentes de biodiversidad en el mundo provienen del suelo, por lo que al considerar la variable biodiversidad en un agroecosistema requiere obligatoriamente involucrar la que está presente en el suelo (Acevedo, 2004).        
+          
+            div(x="0%" y="23%" tooltip="Ver más" numero="1")
+              .tarjeta.white-card.p-4            
+                .row.align-items-center
+                  .col-lg-6
+                    figure
+                      img(src='@/assets/curso/t2-if1.png', alt='imagen decorativa')
+                  .col-lg-6             
+                    p Es un sistema vivo, organizado, con una estructura.      
+
+            div(x="10%" y="65%" tooltip="Ver más" numero="2")
+              .tarjeta.white-card.p-4            
+                .row.align-items-center
+                  .col-lg-6
+                    figure
+                      img(src='@/assets/curso/t2-if2.png', alt='imagen decorativa')
+                  .col-lg-6             
+                    p En él se generan diferentes procesos metabólicos y de ciclaje de nutrientes.  
+
+            div(x="47%" y="92%" tooltip="Ver más" numero="3")
+              .tarjeta.white-card.p-4            
+                .row.align-items-center
+                  .col-lg-6
+                    figure
+                      img(src='@/assets/curso/t2-if3.png', alt='imagen decorativa')
+                  .col-lg-6             
+                    p Es un sistema resiliente, con la capacidad de adaptarse a sucesos extremos. 
+
+            div(x="83%" y="65%" tooltip="Ver más" numero="4")
+              .tarjeta.white-card.p-4            
+                .row.align-items-center
+                  .col-lg-6
+                    figure
+                      img(src='@/assets/curso/t2-if4.png', alt='imagen decorativa')
+                  .col-lg-6             
+                    p Su fertilidad y salud son indispensables en el desarrollo de los cultivos.
+            div(x="95%" y="23%" tooltip="Ver más" numero="5")
+              .tarjeta.white-card.p-4            
+                .row.align-items-center
+                  .col-lg-6
+                    figure
+                      img(src='@/assets/curso/t2-if5.png', alt='imagen decorativa')
+                  .col-lg-6             
+                    p Es uno de los pilares más importantes de la agricultura ecológica y agroecológica y sobre él, se fundamentan quizás las bases más importantes para el diseño de un agroecosistema. Aparentemente, es un recurso estático y muerto, pero guarda una gran riqueza química y biológica que no puede ser vista con el ojo humano.
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .cajon.color-acento-botones.p-4
+          p.mb-0  Otro aspecto de gran importancia para los suelos es la materia orgánica, es decir, la descomposición biológica de residuos orgánicos en el sitio, y que afecta directamente las propiedades físicas y químicas como la estructura, porosidad, infiltración, retención de humedad, biodiversidad y fertilidad, y eso a su vez genera un impacto en la calidad nutricional de los alimentos que se producen.
 
 
 
